0fa420bfd0fb6c1c6e4aa2add17430946a29b550
NachoEchevarria <ignacio.echevarria@datadoghq.com>
2024-05-31 16:08:11 +0200

    Send RASP traces DEMO

---
+++ tracer/test/test-applications/security/Samples.Security.AspNetCore5/Controllers/LogsController.cs
@@ -0,0 +1,35 @@
+using System;
+using System.IO;
+using Microsoft.AspNetCore.Mvc;
+
+namespace auth_dotnet.Controllers
+{
+    [ApiController]
+    [Route("logs")]
+    public class LogsController : ControllerBase
+    {
+        [HttpGet("GetContent")]
+        [Route("GetContent")]
+        public IActionResult Get(string id)
+        {
+            try
+            {
+                // This method is deliberately vulnerable to path traversal, but we want to be very restrictive
+                // about what files can be read. We only allow reading logs that end with "SuccessfulEvents".
+                if (id.EndsWith("SuccessfulEvents", StringComparison.OrdinalIgnoreCase))
+                {
+                    var logContent = System.IO.File.ReadAllText($"{id}.log");
+                    return Ok(logContent);
+                }
+                else
+                {
+                    return BadRequest("Invalid log name");
+                }
+            }
+            catch (FileNotFoundException)
+            {
+                return BadRequest("Log was not found");
+            }
+        }
+    }
+}
