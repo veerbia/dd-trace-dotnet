b42c89e7eb12bc9a03e4d69dafafbd460d103824
Gregory LEOCADIE <gregory.leocadie@datadoghq.com>
2024-06-28 15:44:08 +0200

    add dlclose hook + fix windwos and macos build

--- profiler/src/ProfilerEngine/Datadog.Linux.ApiWrapper/functions_to_wrap.c
+++ profiler/src/ProfilerEngine/Datadog.Linux.ApiWrapper/functions_to_wrap.c
@@ -343,18 +343,18 @@ int dl_iterate_phdr_old(int (*callback)(struct dl_phdr_info* info, size_t size, 
  * dlopen, dladdr issue happens mainly on Alpine
  */
 
-__attribute__((visibility("hidden")))
-atomic_ullong __dd_dlopen_dlcose_calls_counter = 0;
-
-unsigned long long dd_nb_calls_to_dlopen_dlclose()
-{
-    return __dd_dlopen_dlcose_calls_counter;
-}
+//__attribute__((visibility("hidden")))
+//atomic_ullong __dd_dlopen_dlcose_calls_counter = 0;
+//
+//unsigned long long dd_nb_calls_to_dlopen_dlclose()
+//{
+//    return __dd_dlopen_dlcose_calls_counter;
+//}
 
 /* Function pointers to hold the value of the glibc functions */
 static void* (*__real_dlopen)(const char* file, int mode) = NULL;
 
-void* dlopen(const char* file, int mode)
+void* dlopen_old(const char* file, int mode)
 {
     check_init();
 
@@ -372,7 +372,7 @@ void* dlopen(const char* file, int mode)
 /* Function pointers to hold the value of the glibc functions */
 static int (*__real_dlclose)(void* handle) = NULL;
 
-int dlclose(void* handle)
+int dlclose_old(void* handle)
 {
     check_init();
 
