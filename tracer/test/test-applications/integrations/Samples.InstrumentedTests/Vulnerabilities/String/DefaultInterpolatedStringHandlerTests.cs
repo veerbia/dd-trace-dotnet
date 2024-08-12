b8f17b27241b67804987125f90e3124a17c89bb1
NachoEchevarria <ignacio.echevarria@datadoghq.com>
2024-08-12 15:42:15 +0200

    Add instrumented tests

--- tracer/test/test-applications/integrations/Samples.InstrumentedTests/Vulnerabilities/String/DefaultInterpolatedStringHandlerTests.cs
+++ tracer/test/test-applications/integrations/Samples.InstrumentedTests/Vulnerabilities/String/DefaultInterpolatedStringHandlerTests.cs
@@ -1,3 +1,4 @@
+using System.Runtime.CompilerServices;
 using Xunit;
 
 namespace Samples.InstrumentedTests.Iast.Vulnerabilities.StringPropagation;
@@ -6,16 +7,72 @@ public class DefaultInterpolatedStringHandlerTests : InstrumentationTestsBase
     protected string TaintedString = "TaintedString";
     protected string NotTaintedString = "TaintedString333";
 
+    // Not testing generics until they are fully supported:
+    // public void AppendFormatted<T>(T value)
+    // public void AppendFormatted<T>(T value, string? format)
+    // public void AppendFormatted<T>(T value, int alignment)
+    // public void AppendFormatted<T>(T value, int alignment, string? format)
+
     public DefaultInterpolatedStringHandlerTests()
     {
         AddTainted(TaintedString);
     }
 
     [Fact]
-    public void GivenATaintedString_WhenCallingYYY_ResultIsTainted()
+    public void GivenATaintedString_WhenCallingConcatOptimized_ResultIsTainted()
     {
-        var t = "Welcome, " + TaintedString + "eee!";
+        var t = TaintedString + "w";
         var result = $"Hi {TaintedString} {NotTaintedString}.";
         AssertTainted(result);
     }
+
+#if NET6_0_OR_GREATER
+
+    [Fact]
+    public void GivenATaintedString_WhenCallingAppendFormatted_ResultIsTainted()
+    {
+        DefaultInterpolatedStringHandler handler = new();
+        handler.AppendFormatted(TaintedString);
+        var result = handler.ToString();
+        AssertTainted(result);
+    }
+
+    [Fact]
+    public void GivenATaintedString_WhenCallingAppendFormatted_ResultIsTainted2()
+    {
+        DefaultInterpolatedStringHandler handler = new();
+        handler.AppendFormatted(TaintedString, 0);
+        var result = handler.ToString();
+        AssertTainted(result);
+    }
+
+    [Fact]
+    public void GivenATaintedString_WhenCallingAppendFormatted_ResultIsTainted3()
+    {
+        DefaultInterpolatedStringHandler handler = new();
+        handler.AppendFormatted(TaintedString, 0, "format");
+        var result = handler.ToString();
+        AssertTainted(result);
+    }
+
+    [Fact]
+    public void GivenATaintedString_WhenCallingAppendFormatted_ResultIsTainted4()
+    {
+        DefaultInterpolatedStringHandler handler = new();
+        handler.AppendFormatted((object)TaintedString, 0, "format");
+        var result = handler.ToString();
+        AssertTainted(result);
+    }
+
+    [Fact]
+    public void GivenATaintedString_WhenCallingToStringAndClear_ResultIsTainted()
+    {
+        DefaultInterpolatedStringHandler handler = new();
+        handler.AppendFormatted(TaintedString);
+        handler.AppendLiteral("literal");
+        var result = handler.ToStringAndClear();
+        AssertTainted(result);
+    }
+
+#endif
 }
